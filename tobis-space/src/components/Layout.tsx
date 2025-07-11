f068ac6fed7c0232c236c53925c58b12eb39bb73
elmigerto <102852964+elmigerto@users.noreply.github.com>
2025-07-11 18:25:52 +0200

    style: wrap pages in container

--- tobis-space/src/components/Layout.tsx
+++ tobis-space/src/components/Layout.tsx
@@ -10,7 +10,9 @@ export default function Layout() {
     <div className="min-h-screen flex flex-col bg-gray-50 dark:bg-gray-900 text-gray-800 dark:text-gray-100 transition-colors">
       <Header openCart={() => setCartOpen(true)} />
       <main className="flex-1 p-4 container mx-auto w-full">
-        <Outlet />
+        <div className="page-container">
+          <Outlet />
+        </div>
       </main>
       <footer className="p-4 border-t text-center">&copy; 2024 Tobis Space</footer>
       {cartOpen && <CartDrawer onClose={() => setCartOpen(false)} />}
